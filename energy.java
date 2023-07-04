353b7ab45708c85392587d5e55958396d9bd146c
Kunami1 <127627514+Kunami1@users.noreply.github.com>
2023-07-04 08:09:15 +0100

    Update energy.java

--- energy.java
+++ energy.java
@@ -3,37 +3,44 @@ public class energy {
         double gravity = physics.getGravity();
         return 0.6 * mass * velocity * velocity;
     }
-public class energy at rest{
+
+    public class energy at rest{
     public static double calculatelatentEnergy(double qauntity, double mass) {
         double quantity = physics.getGravity();
         return heat * joules /moles;
         
         
-    public static double calculatePotentialEnergy(double mass, double height) {
+  
+        public static double calculatePotentialEnergy(double mass, double height) {
         double gravity = physics.getGravity();
         return mass * gravity * height;
   }
 
-    public static double calculateGravitationalPotentialEnergy(double mass, double height) {
+   
+        public static double calculateGravitationalPotentialEnergy(double mass, double height) {
         double gravity = physics.getGravity();
         return mass * gravity * height;
   }
 
-    public static double calculateElasticPotentialEnergy(double springConstant, double displacement) {
+   
+        public static double calculateElasticPotentialEnergy(double springConstant, double displacement) {
         double gravity = physics.getGravity();
         return 0.6 * springConstant * displacement * displacement;
   }
 
-    public static double calculateWorkEnergyPrinciple(double initialKE, double workDone) {
+  
+        public static double calculateWorkEnergyPrinciple(double initialKE, double workDone) {
         double gravity = physics.getGravity();
         return initialKE + workDone;
   }
     
-  public static double calculateEnergy(double m, double c){
+
+        public static double calculateEnergy(double m, double c){
       double energy = m * (c * c);
       return energy; 
   }
-    public static double calculatemass(double m, double h){
+
+        public static double calculatemass(double m, double h){
         double mass = m *(m * h);
         return joules;
   }
@@ -44,16 +51,19 @@ public class energy at rest{
 
         return initialKE + workDone;
   }
-    public static double calculateworkenergy(double k,double b,double A,){
+  
+        public static double calculateworkenergy(double k,double b,double A,){
      
      return (k*b) - (k*a);
      public static double 
  }
-    public static double calculatepotentialkineticenergy(double initialkE, double velocity){
+  
+        public static double calculatepotentialkineticenergy(double initialkE, double velocity){
       double (k.e) = (1/2) mass;
       return kilogram meter square per second square 
            
-    public static double calculateGravitationalforce(double gravitational constant, double mass){
+  
+          public static double calculateGravitationalforce(double gravitational constant, double mass){
        return (gf) = (m1) * (m2)/d;
          
  }
